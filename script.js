1d381391e71556730d860998b9cb5057e97e1282
YulianaBorys <juna.9258@gmail.com>
2025-03-26 21:18:46 +0200

    Added base files

---
+++ script.js
@@ -0,0 +1,9 @@
+function create(text){
+    let p=document.createElement("p");
+    p.innerHTML=text;
+    document.body.appendChild(p);
+}
+for (let i=1; i<=10; i++){
+    create(`Line ${i}`);
+} 
+
